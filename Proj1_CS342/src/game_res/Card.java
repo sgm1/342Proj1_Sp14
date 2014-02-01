7df6c65aa8609e94d3ced48d40323cffed474c27
sgm1 <sgmathai@att.net>
2014-02-01 13:44:17 -0600

    Lets try this?

---
+++ Proj1_CS342/src/game_res/Card.java
@@ -0,0 +1,5 @@
+package game_res;
+
+public class Card {
+
+}
---
+++ Proj1_CS342/src/launcher/Game.java
@@ -0,0 +1,12 @@
+package launcher;
+
+import game_res.*;
+
+public class Game {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Stuff");
+	}
+
+}
