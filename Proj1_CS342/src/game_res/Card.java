702e9a6af94d99b5ac4c11cfd6134176826cdc0a
sgm1 <sgmathai@att.net>
2014-02-01 17:06:12 -0600

    Did some very basic classes

--- Proj1_CS342/src/game_res/Card.java
+++ Proj1_CS342/src/game_res/Card.java
@@ -1,5 +1,26 @@
 package game_res;
 
 public class Card {
-
+	private int suit;
+	private int rank;
+	
+	@SuppressWarnings("unused")
+	private Card(){}//means no one will use default constructor
+	
+	/**
+	 * 
+	 * @param i The number based on standard 52 card deck (0 - 51)
+	 */
+	public Card(int i){
+	}
+	
+	/**
+	 * 
+	 * @param s Suit number (0 - 3)
+	 * @param r Suit number (0 - 12)
+	 */
+	public Card(int s, int r){
+	}
+	
+	
 }
