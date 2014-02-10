0f0d4a46aadfbca46d8558c1977bd0a054c40287
Krassimir <Krassimir@Krassimir-RIG>
2014-02-10 05:15:33 -0600

    Replace method acting weird, discard phase almost done for use =/

--- Proj1_CS342/src/game_res/Player.java
+++ Proj1_CS342/src/game_res/Player.java
@@ -23,23 +23,33 @@ public class Player {
 		System.out.println("You drew 5 cards.");
 		//winnerThusFar = this;
 	}
-	
+	 /**
+	  * Displays users current hand
+	  */
 	public void showHand() {
 		System.out.print("Your current hand: ");
 		for(int i=0; i<hand; i++){
-			System.out.print(i+1 +") " + hd.getDisplayedCard(i) + "  ");
+			System.out.print(i+1 +") [" + hd.getDisplayedCard(i) + "]  ");
 		}
 		System.out.println("");
 	}
 	
+	/**
+	 * Handles the users discarding phase
+	 * checks for ace, if yes allows user to discard 4 cards,
+	 * otherwise only allows discarding up to 3 cards
+	 * also handles error checking
+	 */
 	public void discardPhase() {
 		boolean ace = false;
+		int aceCount = 0;
 		for(int i=0; i<hand; i++) {
 			Card possibleAce = hd.get(i);
 			if(possibleAce.rank == 14) {
 				System.out.println("You drew an Ace! You can choose to keep " +
 						"the Ace and discard your remaining 4 cards.");
-				ace = true; }
+				ace = true; 
+				aceCount+=1; }
 		}
 		
 		System.out.print("How many cards do you want to discard? : ");
@@ -48,17 +58,31 @@ public class Player {
 			throw new IllegalArgumentException("Can only discard up to three " +
 					"cards without an Ace, and only up to 4 with");
 		
-		System.out.print("List the indices of the cards to discard? : ");
-		for(int i=0; i<numToReplace; i++) {
-			//Java have buffer overflow??
-			int index = stdIn.nextInt();
+		int[] indices = {0,0,0,0,0};
+		if(numToReplace!=0) {
+			System.out.print("List the indices of the cards to discard? : ");
+			for(int i=0; i<numToReplace; i++) {
+				//Java have buffer overflow??
+				int index = stdIn.nextInt()-1;
 			
-			if(index>0 && index<6)
-				hd.replace(i);
-			else throw new IllegalArgumentException("Can only discard cards " +
-				"with and index of 1 through 5");
-			
-			System.out.println("Discarded card at index " + (i+1));
+				if(ace==true && numToReplace==4 && index>(0-1) && index<(4+1)) {
+					Card possibleAce = hd.get(index);
+					if(possibleAce.rank == 14) {
+						aceCount-=1;
+						if(aceCount<1) 
+							throw new IllegalArgumentException("When choosing to " 
+								+ "discard 4 cards, cannot discard last Ace");
+					}
+				}
+				
+				if(index>(0-1) && index<(5+1))
+					indices[i] = index;
+				else throw new IllegalArgumentException("Can only discard cards " 
+					+ "with and index of 1 through 5");
+			    
+				System.out.println("Discarded card at index " + (index+1));
+			}
 		}
+        // need to actually apply replace function.. couldnt figure out how to
 	}
 }
